300d2ca71f57cf19bdb381abae6c7c852d5b997e
Maria Byler <mariabyler98@gmail.com>
2020-11-29 14:11:06 -0500

    fixed bug where inquirer was prompting too soon

--- EmployeeTracker.js
+++ EmployeeTracker.js
@@ -27,8 +27,8 @@ function view() {
     .prompt({
       name: "viewOptions",
       type: "list",
-      message: "Would you like to view a [DEPARTMENT], [ROLE], or [EMPLOYEE]?",
-      choices: ["DEPARTMENT", "ROLE", "EMPLOYEE"]
+      message: "Would you like to view a [DEPARTMENT], [ROLE], [EMPLOYEE], or [NONE]?",
+      choices: ["DEPARTMENT", "ROLE", "EMPLOYEE", "NONE"]
     })
     .then(function(answer) {
       if (answer.viewOptions === "DEPARTMENT") {
@@ -38,11 +38,13 @@ function view() {
         viewRole();
       } else if(answer.viewOptions === "EMPLOYEE") {
         viewEmployee();
-      } else{
-        connection.end();
-      }
+      } else if(answer.viewOptions === "NONE") {
+        add(); 
+      } 
+      // else{
+      //   connection.end(); 
+      // }
     });
-    add(); 
 }
 // function which prompts the user for what action they should take
 function add() {
@@ -50,8 +52,8 @@ function add() {
     .prompt({
       name: "addOptions",
       type: "list",
-      message: "Would you like to add a [DEPARTMENT], [ROLE], or [EMPLOYEE]?",
-      choices: ["DEPARTMENT", "ROLE", "EMPLOYEE"]
+      message: "Would you like to add a [DEPARTMENT], [ROLE], [EMPLOYEE], or [NONE]?",
+      choices: ["DEPARTMENT", "ROLE", "EMPLOYEE", "NONE"]
     })
     .then(function(answer) {
       if (answer.addOptions === "DEPARTMENT") {
@@ -61,11 +63,13 @@ function add() {
         addRole();
       } else if(answer.addOptions === "EMPLOYEE") {
         addEmployee();
-      } else{
-        connection.end();
-      }
+      } else if(answer.addOptions === "NONE") {
+        update(); 
+      } 
+      // else{
+      //   connection.end(); 
+      // }
     });
-    update(); 
 }
 // function which prompts the user for what action they should take
 function update() {
@@ -84,215 +88,96 @@ function update() {
       }
     });
 }
-
-// // function to handle posting new items up for auction
-// function postAuction() {
-//   // prompt for info about the item being put up for auction
-//   inquirer
-//     .prompt([
-//       {
-//         name: "item",
-//         type: "input",
-//         message: "What is the item you would like to submit?"
-//       },
-//       {
-//         name: "category",
-//         type: "input",
-//         message: "What category would you like to place your auction in?"
-//       },
-//       {
-//         name: "startingBid",
-//         type: "input",
-//         message: "What would you like your starting bid to be?",
-//         validate: function(value) {
-//           if (isNaN(value) === false) {
-//             return true;
-//           }
-//           return false;
-//         }
-//       }
-//     ])
-//     .then(function(answer) {
-//       // when finished prompting, insert a new item into the db with that info
-//       connection.query(
-//         "INSERT INTO auctions SET ?",
-//         {
-//           item_name: answer.item,
-//           category: answer.category,
-//           starting_bid: answer.startingBid || 0,
-//           highest_bid: answer.startingBid || 0
-//         },
-//         function(err) {
-//           if (err) throw err;
-//           console.log("Your auction was created successfully!");
-//           // re-prompt the user for if they want to bid or post
-//           start();
-//         }
-//       );
-//     });
-// }
-
-// function bidAuction() {
-//   // query the database for all items being auctioned
-//   connection.query("SELECT * FROM auctions", function(err, results) {
-//     if (err) throw err;
-//     // once you have the items, prompt the user for which they'd like to bid on
-//     inquirer
-//       .prompt([
-//         {
-//           name: "choice",
-//           type: "rawlist",
-//           choices: function() {
-//             var choiceArray = [];
-//             for (var i = 0; i < results.length; i++) {
-//               choiceArray.push(results[i].item_name);
-//             }
-//             return choiceArray;
-//           },
-//           message: "What auction would you like to place a bid in?"
-//         },
-//         {
-//           name: "bid",
-//           type: "input",
-//           message: "How much would you like to bid?"
-//         }
-//       ])
-//       .then(function(answer) {
-//         // get the information of the chosen item
-//         var chosenItem;
-//         for (var i = 0; i < results.length; i++) {
-//           if (results[i].item_name === answer.choice) {
-//             chosenItem = results[i];
-//           }
-//         }
-
-//         // determine if bid was high enough
-//         if (chosenItem.highest_bid < parseInt(answer.bid)) {
-//           // bid was high enough, so update db, let the user know, and start over
-//           connection.query(
-//             "UPDATE auctions SET ? WHERE ?",
-//             [
-//               {
-//                 highest_bid: answer.bid
-//               },
-//               {
-//                 id: chosenItem.id
-//               }
-//             ],
-//             function(error) {
-//               if (error) throw err;
-//               console.log("Bid placed successfully!");
-//               start();
-//             }
-//           );
-//         }
-//         else {
-//           // bid wasn't high enough, so apologize and start over
-//           console.log("Your bid was too low. Try again...");
-//           start();
-//         }
-//       });
-//   });
-// }
-
-
-// creates all queries to database
 // add department
-  // function addDepartment() {
-  //   console.log("Inserting a new department...\n");
-  //   var query = connection.query(
-  //     "INSERT INTO department WHERE department_name ?",
-  //     function(err, res) {
-  //       if (err) throw err;
-  //       console.log(res.affectedRows + " department added!\n");
-  //     }
-  //   );
-  
-  //   // logs the actual query being run
-  //   console.log(query.sql);
-  // } 
-// add role
-//   function addRole() {
-//     console.log("Inserting a new product...\n");
-//     var query = connection.query(
-//       "INSERT INTO products SET ?",
-//       {
-//         flavor: "Rocky Road",
-//         price: 3.0,
-//         quantity: 50
-//       },
-//       function(err, res) {
-//         if (err) throw err;
-//         console.log(res.affectedRows + " product inserted!\n");
-//         // Call updateProduct AFTER the INSERT completes
-//         updateProduct();
-//       }
-//     );
-  
-//     // logs the actual query being run
-//     console.log(query.sql);
-//   }
-// // add employee
-//   function addEmployee() {
-//     console.log("Inserting a new product...\n");
-//     var query = connection.query(
-//       "INSERT INTO products SET ?",
-//       {
-//         flavor: "Rocky Road",
-//         price: 3.0,
-//         quantity: 50
-//       },
-//       function(err, res) {
-//         if (err) throw err;
-//         console.log(res.affectedRows + " product inserted!\n");
-//         // Call updateProduct AFTER the INSERT completes
-//         updateProduct();
-//       }
-//     );
-  
-//     // logs the actual query being run
-//     console.log(query.sql);
-//   }
-  
-//   function updateProduct() {
-//     console.log("Updating all Rocky Road quantities...\n");
-//     var query = connection.query(
-//       "UPDATE products SET ? WHERE ?",
-//       [
-//         {
-//           quantity: 100
-//         },
-//         {
-//           flavor: "Rocky Road"
-//         }
-//       ],
-//       function(err, res) {
-//         if (err) throw err;
-//         console.log(res.affectedRows + " products updated!\n");
-//         // Call deleteProduct AFTER the UPDATE completes
-//         deleteProduct();
-//       }
-//     );
-  
-//     // logs the actual query being run
-//     console.log(query.sql);
-//   }
-  
-//   function deleteProduct() {
-//     console.log("Deleting all strawberry icecream...\n");
-//     connection.query(
-//       "DELETE FROM products WHERE ?",
-//       {
-//         flavor: "strawberry"
-//       },
-//       function(err, res) {
-//         if (err) throw err;
-//         console.log(res.affectedRows + " products deleted!\n");
-//         // Call readProducts AFTER the DELETE completes
-//         readProducts();
-//       }
-//     );
-//   }
-  
+  function addDepartment() {
+    inquirer
+    .prompt({
+      name: "newDept",
+      type: "input",
+      message: "What department would you like to add?",
+    })
+    .then (function(answer) {
+    console.log("Inserting a new department...\n");
+    var query = connection.query(
+      "INSERT INTO department SET ?",
+      {
+        department_name: answer.newDept
+      },
+      
+      function(err, res) {
+        if (err) throw err;
+        console.log(res.affectedRows + " department added!\n");
+        update();
+      }
+      ); 
+    });
+  } 
 
-// // inquirer/command line
+  // add role
+  function addRole() {
+    inquirer
+    .prompt([{
+      name: "newRole",
+      type: "input",
+      message: "What role would you like to add?",
+    }, 
+    {
+      name: "salary",
+      type: "input",
+      message: "What is their salary?",
+      validate: function(value) {
+        if (isNaN(value) === false) {
+          return true;
+        }
+        return false;
+      }
+    }
+    ])
+    .then (function(answer) {
+    console.log("Inserting a new role...\n");
+    var query = connection.query(
+      "INSERT INTO role SET ?",
+      {
+        title: answer.newRole, 
+        salary: answer.salary
+      },
+      
+      function(err, res) {
+        if (err) throw err;
+        console.log(res.affectedRows + " role added!\n");
+        update(); 
+      }
+      ); 
+    });
+  } 
+  // add employee
+  function addEmployee() {
+    inquirer
+    .prompt([{
+      name: "firstName",
+      type: "input",
+      message: "What is the first name of the new employee?"
+    }, 
+    {
+      name: "lastName",
+      type: "input",
+      message: "What is the last name of the new employee?"
+    }
+    ])
+    .then (function(answer) {
+    console.log("Inserting a new employee...\n");
+    var query = connection.query(
+      "INSERT INTO employee SET ?",
+      {
+        first_name: answer.firstName, 
+        last_name: answer.lastName
+      },
+      
+      function(err, res) {
+        if (err) throw err;
+        console.log(res.affectedRows + " employee added!\n");
+        update(); 
+      }
+      ); 
+    }); 
+  } 
